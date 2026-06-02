0469ae5cde644e43dda86ec8b3fd41f08aede998
MD-DD <Mikolajdanicki34@gmail.com>
2026-06-02 15:10:23 +0200

    Gebruikers functionaliteit toegevoegd aan de hand van trello

---
+++ spotify-console-app/src/main/java/nl/mikolajty/User.java
@@ -0,0 +1,23 @@
+package nl.mikolajty;
+
+public class User {
+    
+    private String username;
+    private String playlistName;
+    
+
+    public User(String username){
+        this.username = username;
+    }
+
+
+public String getUsername() {
+    return username;
+}
+
+
+ public String getPlaylistName() {
+        return playlistName;
+ }
+
+}
